ea264551dc013de8fac0ca61513f2fc3951aa1ae
Luis Carlos Quarta <djlevisn1@gmail.com>
2018-11-08 16:15:08 +0100

    finish hello world tests

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num > 1) {
+    return string.repeat(num);
+  } else if (num === 0) {
+    return '';
+  } else if (num < 0) {
+    return 'ERROR';
+  } else {
+    return string;
+  }
 }
 
 module.exports = repeatString
