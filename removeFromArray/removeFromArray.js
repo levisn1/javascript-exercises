9d5013eda26e4a271204430f0ea9e7ce965cf00f
Luis Carlos Quarta <djlevisn1@gmail.com>
2018-11-09 15:05:55 +0100

    removeFromArray works

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, num) {
+  var args = Array.from(arguments).slice(1);
+  return arr.filter(function(val) {
+    return !args.includes(val);
+  });
 }
 
 module.exports = removeFromArray
