47eb6267849fbbd1b7d3bdace74a7a18563e5f52
Luis Carlos Quarta <djlevisn1@gmail.com>
2018-11-09 18:00:47 +0100

    sumAll exercise works

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
-}
+const sumAll = function(a, b) {
+  var foo = [];
+  sum = 0;
+  if ((typeof a === 'string' || typeof b === 'string') || (a < 0 || b < 0) || (typeof a !== 'number' || typeof b !== 'number')) {
+    return 'ERROR'
+  } else {
+    if (a < b) {
+      for (var i = a; i <= b; i++) {
+        foo.push(i);
+      }
+    } else {
+      for (var i = b; i <= a; i++) {
+        foo.push(i);
+      }};
+      return foo.reduce(function(a, b) {
+        return a + b;
+      });
+    }
+  }
 
 module.exports = sumAll
