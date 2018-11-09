f570eecac10104831d5a49c9abfeb7a7c6b5fbff
Luis Carlos Quarta <djlevisn1@gmail.com>
2018-11-10 00:21:01 +0100

    TempConversion works

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(t) {
+  let temp = (t - 32) * 5/9;
+  return Math.round(temp * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(t) {
+  let temp = (t * 9/5) + 32;
+  return Math.round(temp * 10) / 10;
 }
 
 module.exports = {
