025744c53aa67d1b3e07d741a0c31ced10f658b5
Luis Carlos Quarta <djlevisn1@gmail.com>
2018-11-09 19:35:35 +0100

    LeapYears works

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(n) {
+  if (n % 4 !== 0) {
+    return false
+  } else {
+    if (n % 400 !== 0 && n % 100 === 0) {
+      return false
+    } else {
+      return true
+    }
+  }
 }
 
 module.exports = leapYears
+
+
+
