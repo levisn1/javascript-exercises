88ad66d381609e4ed10e3b521af61a4e679b639a
Luis Carlos Quarta <djlevisn1@gmail.com>
2018-11-22 18:14:45 +0100

    ex done

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,52 @@
-function add () {
-	
+function add() {
+	var arr = [...arguments];
+  var sum = arr.reduce(function(total, num) {
+    return total + num;
+  }, 0);
+  return sum;
 }
 
-function subtract () {
-	
+function subtract (a,b) {
+  if (a > b) {
+    return a - b
+  } else {
+    b - a
+  }
 }
 
-function sum () {
-	
+function sum(array) {
+  var arr = array;
+  var sum = arr.reduce(function(total, num) {
+    return total + num;
+  }, 0);
+  return sum;
 }
 
-function multiply () {
-	
+function multiply(array) {
+  var arr = array;
+  var multi = arr.reduce(function(total, num) {
+    return total * num
+  });
+  return multi
 }
 
-function power() {
-	
+function power(a,b) {
+return Math.pow(a, b);
 }
 
-function factorial() {
-	
+function factorial(num) {
+  if (num === 0) {
+    return 1
+  } else {
+    var sum = [];
+    for (var i = 1; i <= num ; i++) {
+      sum.push(i)
+    }
+    var multiply = sum.reduce(function(total, num){
+      return total * num;
+    })
+    return multiply;
+  }
 }
 
 module.exports = {
