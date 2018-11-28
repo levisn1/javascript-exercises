bf5f1d15c0ffaf1cc34bc5c6b3eb17456e825166
Luis Carlos Quarta <djlevisn1@gmail.com>
2018-11-29 00:13:29 +0100

    ex done

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+function fibonacci(num) {
+  if (num > 0) {
+    var fibarray = [1,2,3]
+    for (var i = 3; i < num; i++) {
+      fibarray[i] = (fibarray[i-1] + fibarray[i-2])
+    }
+    return fibarray[num - 2]
+  } else {
+    return 'OOPS'
+  }
 }
 
 module.exports = fibonacci
